8b558973f8b1ad9aaaee920aabe8f00e61073e77
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-03-08 20:25:05 +0530

    header section url correction

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -55,7 +55,7 @@ export default function Header() {
                 <span className="block text-sm">@{currentUser.username}</span>
                 <span className="block text-sm font-medium truncate">{currentUser.email}</span>
               </Dropdown.Header >
-              <Link to={'/dasboard?tab=profile'}>
+              <Link to={'/dashboard?tab=profile'}>
                 <Dropdown.Item>Profile</Dropdown.Item>
               </Link>
               <Dropdown.Divider></Dropdown.Divider>
