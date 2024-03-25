4dd45f814b7cd6a76ad06d6814f4ddccbd37d60a
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-03-25 22:32:20 +0530

    add  admin functionality.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,7 +48,10 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Invalid password"));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
     const { password: pass, ...rest } = validUser._doc;
     res
@@ -67,7 +70,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -85,16 +91,18 @@ export const google = async (req, res, next) => {
           name.toLowerCase().split(" ").join("") +
           Math.random().toString(9).slice(-4),
         email,
-        password:hashedPassword,
-      profilepicture:googlePhotoUrl
+        password: hashedPassword,
+        profilepicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({id:newUser,_id},process.env.JWT_SECRET);
-      const {passwordm, ...rest} = newUser._dec;
-      res.status(200).cookie('access_token',token,{
-        httpOnly:true
-      })
-      .json(rest);
+      const token = jwt.sign({ id: newUser._id ,isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
+      const { passwordm, ...rest } = newUser._dec;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
     }
   } catch (error) {}
 };
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
           default:"https://tse3.mm.bing.net/th?id=OIP.m_E2GiWXc8IGEeYAbypLgAHaHa&pid=Api&P=0&h=180",
 
     },
+    isAdmin:{
+        type: Boolean,
+        default:false
+    },
     },{timestamps:true} // when user created .
 
 );
