7460893c569669d87a4b81ab650343b256a7fff7
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-04-03 12:17:10 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+
+  const [showMore, setShowMore] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -14,6 +15,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -23,9 +27,27 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async ()=>{
+    const startIndex = userPosts.length;
+    try {
+       const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`) ;
+       const data = await res.json();
+       if(res.ok){
+        setUserPosts((prev)=>[...prev, ...data.posts]);
+        if(data.posts.length<9){
+          setShowMore(false);
+        }
+       }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 dark: scrollbar-track-slate-700 dark: scrollbar-thumb-slate-500
-    ">
+    <div
+      className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 dark: scrollbar-track-slate-700 dark: scrollbar-thumb-slate-500
+    "
+    >
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
@@ -55,21 +77,32 @@ export default function DashPosts() {
                     </Link>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className="font-medium text-gray-900 dark:text-white" to={`/post/${post.slug}`}>{post.title}</Link>
+                    <Link
+                      className="font-medium text-gray-900 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
                     <span className=" font-medium text-red-500">Delete</span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className="font-bold text-teal-500" to={`/update-post/${post._id}`}>
-                    <span>Edit</span>
+                    <Link
+                      className="font-bold text-teal-500"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
                     </Link>
                   </Table.Cell>
                 </Table.Row>
               ))}
             </Table.Body>
           </Table>
+          {showMore && (
+            <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7" >Show more</button>
+          )}
         </>
       ) : (
         <p>You have no posts yet.</p>
