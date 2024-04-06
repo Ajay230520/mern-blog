9932615bcb4c2a23a9241c27b771b36766764695
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-04-06 13:48:44 +0530

    show users to the admin dashboard.

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from "react-icons/hi";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -61,6 +61,20 @@ export default function DashSidebar() {
               Posts
             </Sidebar.Item>
           </Link>
+          
+          )}
+          {currentUser.isAdmin && (
+
+          <Link to="/dashboard?tab=users">
+            <Sidebar.Item
+              active={tab === 'users'}
+              icon={HiOutlineUserGroup}
+              as="div"
+            >
+              Users
+            </Sidebar.Item>
+          </Link>
+          
           )}
           <Sidebar.Item icon={HiArrowSmRight} onClick={handleSignOut}>
             Sign Out 
---
+++ client/src/components/DashUsers.jsx
@@ -0,0 +1,147 @@
+import { Table, Modal, Button } from "flowbite-react";
+import React from "react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import {FaCheck, FaTimes} from 'react-icons/fa'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export default function DashUsers() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [users, setUsers] = useState([]);
+  const [showModel, setShowModel] = useState(false);
+  const [showMore, setShowMore] = useState(true);
+  const [userIdToDelete, setUserIdToDelete] = useState();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`/api/user/getusers`);
+        const data = await res.json();
+        if (res.ok) {
+          setUsers(data.users);
+          if (data.users.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchUsers();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = users.length;
+    try {
+      const res = await fetch(
+        `/api/user/getusers?startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => [...prev, ...data.users]);
+        if (data.users.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+const handleDeleteUser = async (e)=>{
+
+}
+  return (
+    <div
+      className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 dark: scrollbar-track-slate-700 dark: scrollbar-thumb-slate-500
+    "
+    >
+      {currentUser.isAdmin && users.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date created</Table.HeadCell>
+              <Table.HeadCell>User image</Table.HeadCell>
+              <Table.HeadCell>username </Table.HeadCell>
+              <Table.HeadCell>Email </Table.HeadCell>
+
+              <Table.HeadCell>Admin</Table.HeadCell>
+              <Table.HeadCell>delete</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {users.map((user) => (
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={user._id}>
+                  <Table.Cell>
+                    {new Date(user.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                   
+                      <img
+                        src={user.profilepicture}
+                        className="w-12 h-12 object-cover bg-gray-500 "
+                        alt={user.username}
+                      />
+                  </Table.Cell>
+                  <Table.Cell>
+                    {user.username}
+                  </Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell>{user.isAdmin ? <FaCheck className="text-green-500"/> : <FaTimes className="text-red-500"/>}</Table.Cell>
+
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowModel(true);
+                        setUserIdToDelete(user._id);
+                      }}
+                      className="cursor-pointer font-medium text-red-500"
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+      <Modal
+        show={showModel}
+        onClose={() => setShowModel(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this User?.
+            </h3>
+            <div className="flex justify-between">
+              <Button color="failure" onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button onClick={() => setShowModel(false)} color="gray">
+                No,cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
