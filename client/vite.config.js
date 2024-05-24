1ec94014baa05cbde231f38c5a0ec8cf28ececf3
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-05-24 16:04:41 +0530

    URl_Change_For_Upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
    server: {
       proxy: {
         '/api' :{
-          target: 'http://localhost:3000',
+          target: 'https://mern-blog-dgue.onrender.com',
           secure: false,
         },
       },
