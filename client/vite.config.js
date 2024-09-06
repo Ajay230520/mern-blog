0b1a61a111e4537f9bf2e19746b8282c0eaba548
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-09-06 17:55:21 +0530

    fix-Configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 
-const serverOn = import.meta.env.NODE_ENV;
+// const serverOn = import.meta.env.NODE_ENV;
+const viteApiUrl = import.meta.env.VITE_API_URL;
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target:'http://localhost:3000', // use http for local development
+        target: viteApiUrl ? viteApiUrl :'http://localhost:3000', // use http for local development
         changeOrigin: true,
         secure: false,
       },
