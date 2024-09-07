a919218a81be6481e6be2bee628ce6ccc760d9dc
Ajay230520 <ajaybhaimuniya@gmail.com>
2024-09-07 11:22:57 +0530

    Setup LInk

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoute from './routes/comment.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoute from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import path from 'path';
-import cors from 'cors';
+import path from "path";
+import cors from "cors";
 dotenv.config();
 
- 
 const URL = process.env.MONGO_URL;
-mongoose.connect(URL)
+mongoose
+  .connect(URL)
   .then(() => {
     console.log("mongo db is connected!!");
   })
@@ -25,32 +25,31 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow methods
-}));
+app.use(
+  cors({
+    origin: "https://ajayblog-ctc7.onrender.com", // Allow requests from this origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow methods
+  })
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
 
-
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
-app.use('/api/post',postRoutes);
-app.use('/api/comment',commentRoute);
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.html'));
-})
-app.use((err,req,res,next) => {
-  const  statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal server Error';
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoute);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server Error";
   res.status(statusCode).json({
-
-    success : false,
+    success: false,
     statusCode,
-    message
-  }
-  )
+    message,
+  });
 });
